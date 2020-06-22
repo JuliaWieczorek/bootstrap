548397f2fa0ecede3a8c8cd44a0c488c8096f5f7
JuliaWieczorek <wieczorek.julia.76@gmail.com>
2020-06-22 23:20:16 +0200

    Initial commit

---
+++ R/bootstrap.R
@@ -0,0 +1,26 @@
+#' @title A bootstrap function
+#'
+#' @description This functions allows you to estimate the value of the estimator
+#' by means of multiple draws with returning from a sample.
+#'
+#' @param X data vector, np. c(1,2,3)
+#' @param n number of bootstrap repetitions
+#' @param ES estimation function, np. mean, var, sd or function create by you
+#'
+#' @keywords bootstrap
+#'
+#' @example
+#' x = c(138.5, 138.5, 140.0, 141.0, 141.0, 143.5, 145.0, 147.0, 148.5, 150.0, 153.0, 154.0, 155.0, 156.5, 157.0, 158.5, 159.0, 159.0, 159.0, 160.5, 161.0, 162.0)
+#' bootstrap(x, 200, mean)
+#' bootstrap(x, 500, mean)
+#' bootstrap(x, 1000, mean)
+#' bootstrap(x, 10000, mean)
+
+
+bootstrap <- function(X, n, ES){
+  mi <- mean(X)
+  boot <- lapply(1:n, function(i) sample(X, replace = T))
+  e <- sapply(boot, ES)
+  return (e)
+}
+
